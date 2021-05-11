8f51477d1deb6770e89027a5106abbf4de93805f
yinhui-evelyn <71414819+evelynliyh@users.noreply.github.com>
2021-05-11 10:36:32 +0800

    Update SQLExec.cpp

--- SQLExec.cpp
+++ SQLExec.cpp
@@ -367,10 +367,8 @@ QueryResult *SQLExec::create_index(const CreateStatement *statement) {
     Identifier column_name;
     ValueDict row;
 
-    cout << "test1" << endl;
-
     // get index type
-    try {
+    try { 
         index_type = statement->indexType;
         is_unique = false;
     } catch(exception &e) {
@@ -378,15 +376,12 @@ QueryResult *SQLExec::create_index(const CreateStatement *statement) {
         is_unique = true;
     }
 
-    cout << "test2" << endl;
-
     row["table_name"] = Value(table_name);
     row["index_name"] = Value(index_name);
     row["seq_in_index"] = Value(0);
     row["index_type"] = Value(index_type);
     row["is_unique"] = Value(is_unique);
 
-    cout << "test3" << endl;
     //set indices to rows
     for (auto const &col_name : *statement->indexColumns) {
         if (!columnSet.count(Value(std::string(col_name)))) {
@@ -396,12 +391,10 @@ QueryResult *SQLExec::create_index(const CreateStatement *statement) {
         row["seq_in_index"] = Value(row["seq_in_index"].n + 1);
         Handle handle = SQLExec::indices->insert(&row);
     }
-    cout << "test4" << endl;
     // create index
     DbIndex &index = SQLExec::indices->get_index(table_name, index_name);
     index.create();
 
-    cout << "test5" << endl;
     return new QueryResult(std::string("Created index ") + index_name);
 }
 
